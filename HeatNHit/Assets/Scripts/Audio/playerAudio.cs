297f67d0bac6d74e04041f7eaa9d6cc189fbaf4f
OriolPerarnauArnau <48680365+OriolPerarnauArnau@users.noreply.github.com>
2019-12-21 19:15:09 +0100

    començament Audio Player

---
+++ HeatNHit/Assets/Scripts/Audio/playerAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Oriol Perarnau Arnau
+public class playerAudio : MonoBehaviour
+{
+
+    //Class to manage better all audio clips of player
+
+    public AudioSource hurtSound;                      //Sound of shooting
+
+    public void playHurt()
+    {
+        //Plays shoot sound
+
+        hurtSound.Play();
+    }
+}
+
